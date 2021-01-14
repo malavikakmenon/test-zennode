f991012bf67bea16353d5f24337be1343e7c6c82
malavikakmenon <malvikamalz@gmail.com>
2021-01-14 23:03:46 +0530

    final changes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
         $(".shapes-input").hide();
         $(".square-area").show();
         $(".area-answer").html("");
+        // $(".circle-area input, .triangle-area input, .rectangle-area input").val("");
+
         // alert("You entered p1!");
     })
 
@@ -13,6 +15,9 @@ $(document).ready(function(){
         $(".shapes-input").hide();
         $(".triangle-area").show();
         $(".area-answer").html("");
+        // $(".circle-area input, .square-area input, .rectangle-area input").val("");
+        
+
         // alert("You entered p1!");
     })
     // rectangle
@@ -20,6 +25,9 @@ $(document).ready(function(){
         $(".shapes-input").hide();
         $(".rectangle-area").show();
         $(".area-answer").html("");
+        // $(".circle-area input, .triangle-area input, .square-area input").val("");
+      
+
         // alert("You entered p1!");
     })
     // circle
@@ -27,6 +35,9 @@ $(document).ready(function(){
         $(".shapes-input").hide();
         $(".circle-area").show();
         $(".area-answer").html("");
+        // $(".square-area input, .triangle-area input, .rectangle-area input").val("");
+
+
         // alert("You entered p1!");
     })
 
@@ -40,7 +51,17 @@ $(document).ready(function(){
         // alert(radius);
         var circlearea =3.14*radius*radius;
         // alert(circlearea);
+        var arealimit = $(".circle-area .area-limit").val();
+        if(circlearea < arealimit){
+            $(".area-answer").css("color","red") ;
+          }
+          else{
+            $(".area-answer").css("color","green") ;
+          }
+
+
         $(".area-answer").html("Area of Circle is "+circlearea);
+        
 
     })
 
@@ -51,7 +72,17 @@ $(document).ready(function(){
         // alert(length);
         var squarearea = length*length;
         // alert(squarearea);
+        var arealimit = $(".square-area .area-limit").val();
+
+        if(squarearea < arealimit){
+            $(".area-answer").css("color","red") ;
+          }
+          else{
+            $(".area-answer").css("color","green") ;
+          }
+        
         $(".area-answer").html("Area of square is "+squarearea);
+
 
     })
      // triangle area
@@ -62,6 +93,15 @@ $(document).ready(function(){
         // alert(length);
         var trianglearea = 0.5*base*height;
         // alert(trianglearea);
+        var arealimit = $(".triangle-area .area-limit").val();
+
+        if(trianglearea < arealimit){
+            $(".area-answer").css("color","red") ;
+          }
+          else{
+            $(".area-answer").css("color","green") ;
+          }
+        
         $(".area-answer").html("Area of triangle is "+trianglearea);
 
     })
@@ -73,7 +113,25 @@ $(document).ready(function(){
         // alert(length);
         var rectanglearea = lengthr*breadth;
         // alert(rectanglearea);
+        var arealimit = $(".rectangle-area .area-limit").val();
+
+        if(rectanglearea < arealimit){
+            $(".area-answer").css("color","red") ;
+          }
+          else{
+            $(".area-answer").css("color","green") ;
+          }
+        
         $(".area-answer").html("Area of rectangle is "+rectanglearea);
+
+    })
+    $(".parameter-wrapper button").click(function(e){
+      e.preventDefault();
+      $(".answer-wrapper").slideDown();
+
+    })
+    $(".form-check").click(function(){
+      $(".answer-wrapper").slideUp();
 
     })
 
